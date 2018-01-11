26a089444fb1e5c8a97022162ee40ed05e1e130e
Zakir Butte <zakir.butte@gmailcom>
2018-01-11 01:50:57 -0800

    added match alert

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardsInPlay = [];
+
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	}
+	else alert("Sorry, try again.")
+}
