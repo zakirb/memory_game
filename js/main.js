385c197dea2274238a4882f36c6b1f599388085c
Zakir Butte <zakir.butte@gmailcom>
2018-01-11 04:12:52 -0800

    added functions

--- js/main.js
+++ js/main.js
@@ -3,22 +3,27 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped queen");
-
-cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!")
+		} else {
+			alert("Sorry, try again.")
+		}
 	}
-	else alert("Sorry, try again.")
 }
+
+var flipCard = function (cardId) {
+
+	console.log("User flipped " + cardId);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+	flipCard(0);
+	flipCard(2);
+
+
+
+
