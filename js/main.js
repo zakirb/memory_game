baddd72b94e287d7cbb5dcd13889501f56f7e746
Zakir Butte <zakir.butte@gmailcom>
2018-01-12 13:40:43 -0800

    finished project but needs debugging

--- js/main.js
+++ js/main.js
@@ -26,17 +26,18 @@ var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay.length === 2) {
-
+		this.setAttribute('src', cards[cardId].cardImage);
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!")
 		} else {
 			alert("Sorry, try again.")
 		}
 	}
-}
+};
 
-var flipCard = function (cardId) {
+var flipCard = function () {
 
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -44,8 +45,18 @@ var flipCard = function (cardId) {
 	checkForMatch();
 };
 
-	flipCard(0);
-	flipCard(2);
+var createBoard = function() {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		document.cardElement.setAttribute('src', "images/back.png");
+		document.cardElement.setAttribute('data-id', i);
+		document.cardElement.addEventListener('click', flipcard);
+		document.querySelector('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
 
 
 
